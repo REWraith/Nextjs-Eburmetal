09fdaafeaed3bba1f52ac914636615829f5cb3e1
Mustafa Alparslan <mustibatualp@gmail.com>
2024-12-01 14:53:12 +0300

    tab icon ve title eklendi

--- pages/_app.js
+++ pages/_app.js
@@ -1,16 +1,30 @@
+import React,{useEffect} from 'react'
 import Head from 'next/head'
 import '../styles/global.css'
 
 export default function MyApp ({ Component, pageProps }) {
+  useEffect(() => {
+    var Tawk_API = Tawk_API || {},
+      Tawk_LoadStart = new Date()
+    ;(function () {
+      var s1 = document.createElement('script'),
+        s0 = document.getElementsByTagName('script')[0]
+      s1.async = true
+      s1.src = 'https://embed.tawk.to/6728cf444304e3196adcde77/1ibrmj97c'
+      s1.charset = 'UTF-8'
+      s1.setAttribute('crossorigin', '*')
+      s0.parentNode.insertBefore(s1, s0)
+    })()
+  }, [])
   return (
     <>
       <Head>
-        <title>Division Company</title>
+        <title>Ebru Metal</title>
         <meta
           name='description'
-          content='Division, mobil uygulama geliştirme hizmetlerinde lider bir şirkettir.'
+          content='Ebru Metal, metal işleme ve endüstriyel çözümler sunan lider bir firmadır.'
         />
-        {/* <link rel='icon' href='/images/footerlogo.png' type='image/png' /> */}
+        <link rel='icon' href='/assets/logo/1.png' type='image/png' />
       </Head>
       <Component {...pageProps} />
     </>
